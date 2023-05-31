34fa4bcd119ffc7941ad538ab52a9936f65b6a5b
malvind007 <malvinholdings@gmail.com>
2023-05-31 10:21:27 +0200

    learning tillwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+      screens:{
+        lg: "1124px",
+        xl: "1124px",
+        "2xl":"1124px",      
+      },
+    },
+  },
+  plugins: [],
+}
+
