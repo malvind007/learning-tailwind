be6a2d696af3e7b6ae85d9abc6aa5b436082ae21
malvind007 <malvinholdings@gmail.com>
2023-06-06 13:36:46 +0200

    first tailwind project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-  ],
+  content: ['./*.html'],
   theme: {
-    extend: {},
-    container: {
-      center: true,
-      padding: "1rem",
-      screens:{
-        lg: "1124px",
-        xl: "1124px",
-        "2xl":"1124px",      
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
       },
     },
   },
   plugins: [],
 }
-
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    entry: './src/index.html',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.html',
-    },
-};
